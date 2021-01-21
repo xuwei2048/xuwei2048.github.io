6bcd7b87e1291725701e58492da6e317b14aca9c
admin <weixu_2013@163.com>
2021-01-22 01:49:40 +0800

    demo

--- upload.sh
+++ upload.sh
@@ -4,4 +4,8 @@ git add .
 git commit -m "demo"
 git pull origin master
 git push origin master
-expect input.sh
+#expect input.sh
+expect "Username for 'https://github.com':"
+send "xuwei2048\n"
+expect "Password for 'https://xuwei2048@github.com':"
+send "Xu556789\n"
